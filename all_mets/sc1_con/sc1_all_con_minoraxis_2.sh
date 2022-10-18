6a8b33991c7cc213c50cf74e193c562ba9ad78f9
Ye Bi <78588679+yebigithub@users.noreply.github.com>
2022-10-18 14:47:39 -0400

    Add files via upload

---
+++ all_mets/sc1_con/sc1_all_con_minoraxis_2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+### sc1_all_con_minoraxis_2.sh   ### <---------- Change me
+###########################################################################
+## environment & variable setup
+####### job customization
+#SBATCH --job-name="sc1_all_con_minoraxis_2"   ### <---------- Change me
+#SBATCH -N 3
+#SBATCH -n 16
+#SBATCH -t 10:00:00
+#SBATCH -p normal_q
+#SBATCH --mem-per-cpu=32G
+#SBATCH -A multiomicquantgen    #### <------- change me
+####### end of job customization
+# end of environment & variable setup
+###########################################################################
+#### add modules on TC/Infer
+module load containers/singularity/3.8.5
+### from DT/CA, use module load singularity
+module list
+#end of add modules
+###########################################################################
+###print script to keep a record of what is done
+cat sc1_all_con_minoraxis_2.sh  ### <---------- Change me
+###########################################################################
+echo start running R
+## note, on DT/CA, you should replace projects with groups
+
+singularity exec --bind=/work,/projects \
+    /projects/arcsingularity/ood-rstudio141717-bio_4.1.0.sif Rscript sc1_all_con_minoraxis_2.R   ### <---------- Change me
+
+exit;
---
+++ all_mets/sc1_trt/sc1_all_trt_perimeter.R
@@ -0,0 +1,78 @@
+# knitr::opts_chunk$set(echo = TRUE)
+# knitr::opts_knit$set(root.dir = "~/OneDrive - Virginia Tech/Research/Codes/research/RiceUNLMetabolites/Prediction/Prediction/Multi_trait")
+# setwd("~/Library/CloudStorage/OneDrive-VirginiaTech/Research/Codes/research/RiceUNLMetabolites/Prediction/Multi_trait")
+path.out = "./outputs"
+path.geno = "../../Geno"
+
+library(tidyverse)
+library(BGLR)
+
+
+# load(file=file.path(path.geno, "geno.rr.con.RData")) # 192 385118
+load(file=file.path(path.geno, "geno.rr.trt.RData")) # 188 389854
+
+
+Gcs_trt = scale(geno_trt, scale=T, center = T)
+Gchnt_trt = tcrossprod(Gcs_trt)/ncol(Gcs_trt)
+EVD_Gchnt_trt <- eigen(Gchnt_trt)
+
+
+
+multi_func<-function(group, nCV, mets){
+        nCV=nCV
+        # CorL <- list()
+
+        for (p in 1:12){
+        met_trait = read.csv(paste0("./CrossValidation/cv_",p,"/met_trait_scale_",p,".csv"))
+        # met_trait <- read.csv(file="./CrossValidation/cv_1/met_trait_scale_1.csv")
+        group.df <- met_trait %>% filter(Treatment==group)
+        # group.df <- cbind(group.df, pcaL[[group]])
+
+        # traits = c("MajorAxis", "MinorAxis", "Perimeter")
+        
+        
+        traits = "Perimeter" ##################change here
+        
+        
+        mets = mets
+        # CorR <- NULL
+        CorR <- matrix(nrow = length(traits), ncol = length(mets), dimnames = list(traits,mets))
+        for (i in traits){
+          for (j in mets){
+            set.seed(100+p)
+            cat("Now running Treatment_Trait_Met_CV: ", group, i, j, p, "\n")
+            Y0 = cbind(scale(group.df[,i]), group.df[,j]) #just scale traits, no scale mets.
+            Y <- Y0
+            index = which(group.df$set == "test")
+            Y[index, 1] = NA
+            # table(is.na(y[,1]))
+            # table(is.na(y[,2]))
+
+
+            if(group == "Control"){
+            ETA <- list(
+                G = list(K=Gchnt_con, model='RKHS')
+            )} else{
+            ETA <- list(
+                G = list(K=Gchnt_trt, model='RKHS')
+            )
+            }
+
+            fm <- Multitrait(y=Y,ETA=ETA,verbose=F, nIter = 30000, burnIn = 10000, thin = 5 )
+            pred = fm$ETAHat[index,]
+            real = Y0[index, ]
+            CorR[i,j] = cor(pred, real)[1,1]
+
+          }
+        }
+        # CorL[[p]] = CorR
+        path0 = paste0("./outputs/sc1/", group, "_", i, "_cv",p)
+        dir.create(file.path(path0), recursive = T)
+        saveRDS(CorR, file=paste0(path0, "/scenario1_all", ".rds"))
+
+      }
+
+}
+mets0= c(paste0("a",1:10),paste0("b",1:10),paste0("c",1:10),paste0("d",1:10),paste0("e",1:10),paste0("f",1:10),paste0("g",1:10),paste0("h",1:3))
+mets = mets0
+multi_func(group="Stress", nCV=25, mets = mets)
